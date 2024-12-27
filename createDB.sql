13297d787f912c25ac6953d243e748ecf5667c77
egapay <etg5588@rit.edu>
2024-12-26 19:20:41 -0500

    recreated database, fixed python cleaning

--- createDB.sql
+++ createDB.sql
@@ -1,20 +1,43 @@
-CREATE TABLE hands (
-    dealer_up INTEGER,
-    initial_hand TEXT,
-    dealer_final TEXT,
-    dealer_final_value TEXT,
-    player_final TEXT,
-    player_final_value TEXT,
-    actions_taken TEXT,
-    win NUMERIC
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+
+CREATE TABLE round(
+	round_id BIGSERIAL,
+	win DECIMAL,
+	PRIMARY KEY(round_id)
 );
 
--- COPY hands (dealer_up, initial_hand, dealer_final, 
--- 				dealer_final_value, player_final, player_final_value, actions_taken, win)
--- FROM './blackjack_simulator.csv'
--- WITH (FORMAT csv, HEADER true);
+CREATE TABLE dealer(
+	round_id BIGSERIAL,
+	dealer_up SMALLINT,
+	dealer_final SMALLINT[],
+	dealer_final_value SMALLINT,
+	PRIMARY KEY(round_id),
+	CONSTRAINT fk_dealer_round 
+		FOREIGN KEY(round_id)
+			REFERENCES round(round_id)
+);
 
+CREATE TABLE player(
+	round_id BIGSERIAL,
+	hand_id SERIAL,
+	player_final SMALLINT[],
+	player_final_value SMALLINT,
+	actions_taken text[],
+	PRIMARY KEY(round_id, hand_id),
+	CONSTRAINT fk_player_round
+		FOREIGN KEY(round_id)
+			REFERENCES round(round_id)
+);
 
--- INSERT INTO hands (dealer_up, initial_hand, dealer_final, 
--- 					dealer_final_value, player_final, player_final_value, actions_taken, win)
--- VALUES (10, ARRAY[10,11], ARRAY[10, 4, 10], 24, ARRAY[ARRAY[10, 11], ARRAY[9,11]], ARRAY['BJ'], ARRAY[ARRAY['S']], 1.5)
+CREATE TABLE player_start(
+	round_id BIGSERIAL,
+	player_initial SMALLINT[],
+	PRIMARY KEY(round_id),
+	CONSTRAINT fk_player_start_round
+		FOREIGN KEY(round_id)
+			REFERENCES round(round_id)
+);
