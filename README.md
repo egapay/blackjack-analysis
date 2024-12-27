f40f97caaf4df9b9632933c38994802f3bbf5ed9
egapay <etg5588@rit.edu>
2024-12-26 19:24:30 -0500

    recreated database, fixed python cleaning

--- README.md
+++ README.md
@@ -16,4 +16,7 @@
 2. Move to repo folder
 3. Run clean_csv.py
 4. Run command in psql console:
-   1. `\copy hands (dealer_up, initial_hand, dealer_final, dealer_final_value, player_final, player_final_value, actions_taken, win) FROM 'REPLACE WITH PATH TO CSV IN REPO' WITH (FORMAT csv, HEADER true);`
+   1. `\copy round (round_id, win) FROM 'REPLACE WITH PATH TO CSV IN REPO' WITH (FORMAT csv, HEADER true);`
+   2. `\copy dealer (round_id, dealer_up, dealer_final, dealer_final_value) FROM 'REPLACE WITH PATH TO CSV IN REPO' WITH (FORMAT csv, HEADER true);`
+   3. `\copy player (round_id, player_final, player_final_value, actions_taken) FROM 'REPLACE WITH PATH TO CSV IN REPO' WITH (FORMAT csv, HEADER true);`
+   4. `\copy player_start (round_id, player_initial) FROM 'REPLACE WITH PATH TO CSV IN REPO' WITH (FORMAT csv, HEADER true);`
--- clean_csv_toXlsx.py
+++ clean_csv_toXlsx.py
@@ -1,15 +1,40 @@
 import pandas as pd
+import ast
 
 if __name__ == "__main__":
-    df = pd.read_csv("./blackjack_simulator.csv", nrows=1000000)
+    df = pd.read_csv("./blackjack_simulator.csv", nrows=1000)
 
-    filter_columns = ['dealer_up', 'initial_hand', 'dealer_final', 'dealer_final_value', 'player_final', 'player_final_value', 'actions_taken', 'win']
+    # filter_columns = ['dealer_up', 'initial_hand', 'dealer_final', 'dealer_final_value', 'player_final', 'player_final_value', 'actions_taken', 'win']
+    round_table = ['win']
+    dealer_table = ['dealer_up', 'dealer_final', 'dealer_final_value']
+    player_table = ['player_final', 'player_final_value', 'actions_taken']
+    player_start_table = ['initial_hand']
 
-    filtered_df = df[filter_columns]
-    filtered_df.loc[:, 'initial_hand'] = filtered_df['initial_hand'].str.replace('[', '').str.replace(']', '')
-    filtered_df.loc[:, 'dealer_final'] = filtered_df['dealer_final'].str.replace('[', '').str.replace(']', '')
-    filtered_df.loc[:, 'player_final'] = filtered_df['player_final'].str[1:-1]
-    filtered_df.loc[:, 'player_final_value'] = filtered_df['player_final_value'].str.replace('[', '').str.replace(']', '')
-    filtered_df.loc[:, 'actions_taken'] = filtered_df['actions_taken'].str[1:-1]
 
-    filtered_df.to_excel("./cleaned_blackjack.xlsx", index=False)
+    round_df = df[round_table]
+    
+    dealer_df = df[dealer_table]
+    dealer_df.loc[:, 'dealer_final'] = dealer_df['dealer_final'].apply(ast.literal_eval)
+    dealer_df.loc[:, 'dealer_final'] = dealer_df['dealer_final'].astype(str).str.replace('[', '{').str.replace(']', '}')
+    dealer_df.loc[:, 'dealer_final_value'] = dealer_df['dealer_final_value'].str.replace('BJ', '21')
+
+    player_df = df[player_table]
+    player_df.loc[:, 'player_final'] = player_df['player_final'].apply(ast.literal_eval)
+    player_df.loc[:, 'player_final_value'] = player_df['player_final_value'].apply(ast.literal_eval)
+    player_df.loc[:, 'actions_taken'] = player_df['actions_taken'].apply(ast.literal_eval)
+
+    player_df = player_df.explode(['player_final', 'player_final_value', 'actions_taken'])
+
+    player_df.loc[:, 'player_final'] = player_df['player_final'].astype(str).str.replace('[', '{').str.replace(']', '}')
+    player_df.loc[:, 'player_final_value'] = player_df['player_final_value'].astype(str).str.replace('[', '{').str.replace(']', '}').str.replace('BJ', '21')
+    player_df.loc[:, 'actions_taken'] = player_df['actions_taken'].astype(str).str.replace('[', '{').str.replace(']', '}')
+
+    player_start_df = df[player_start_table]
+    player_start_df.rename(columns={"initial_hand": "player_initial"})
+    player_start_df.loc[:, 'initial_hand'] = player_start_df['initial_hand'].apply(ast.literal_eval)
+    player_start_df.loc[:, 'initial_hand'] = player_start_df['initial_hand'].astype(str).str.replace('[', '{').str.replace(']', '}')
+
+    round_df.to_excel("./round.xlsx", index=True)
+    dealer_df.to_excel("./dealer.xlsx", index=True)
+    player_df.to_excel("./player.xlsx", index=True)
+    player_start_df.to_excel("./player_start.xlsx", index=True)
