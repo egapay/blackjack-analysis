c23e725a2d34fd49177cbcd75296fcb51123d21a
ethangapay <ethantgapay@gmail.com>
2025-01-01 19:29:24 -0500

    finished data modeling/db design section

--- src/py/clean_csv.py
+++ src/py/clean_csv.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import ast
-import ast
 
 if __name__ == "__main__":
-    df = pd.read_csv("./blackjack_simulator.csv", nrows=5000000)
+    df = pd.read_csv("./blackjack_simulator.csv", nrows=5000000) #limiting to 5 million rows
 
-    # filter_columns = ['dealer_up', 'initial_hand', 'dealer_final', 'dealer_final_value', 'player_final', 'player_final_value', 'actions_taken', 'win']
+    # Splitting the columns into separate tables
     round_table = ['win']
     dealer_table = ['dealer_up', 'dealer_final', 'dealer_final_value']
     player_table = ['player_final', 'player_final_value', 'actions_taken']
     player_start_table = ['initial_hand']
 
-
     round_df = df[round_table]
     
     dealer_df = df[dealer_table]
     dealer_df.loc[:, 'dealer_final'] = dealer_df['dealer_final'].apply(ast.literal_eval)
+    # changing [ to { and ] to } to allow for list type in postgres
     dealer_df.loc[:, 'dealer_final'] = dealer_df['dealer_final'].astype(str).str.replace('[', '{').str.replace(']', '}')
-    dealer_df.loc[:, 'dealer_final_value'] = dealer_df['dealer_final_value'].str.replace('BJ', '21')
+    dealer_df.loc[:, 'dealer_final_value'] = dealer_df['dealer_final_value'].str.replace('BJ', '21') # BJ can be inferred by 1.5 win
 
     player_df = df[player_table]
     player_df.loc[:, 'player_final'] = player_df['player_final'].apply(ast.literal_eval)
@@ -26,12 +25,13 @@ if __name__ == "__main__":
 
     player_df = player_df.explode(['player_final', 'player_final_value', 'actions_taken'])
 
+    # changing [ to { and ] to } to allow for list type in postgres
     player_df.loc[:, 'player_final'] = player_df['player_final'].astype(str).str.replace('[', '{').str.replace(']', '}')
     player_df.loc[:, 'player_final_value'] = player_df['player_final_value'].astype(str).str.replace('[', '{').str.replace(']', '}').str.replace('BJ', '21')
     player_df.loc[:, 'actions_taken'] = player_df['actions_taken'].astype(str).str.replace('[', '{').str.replace(']', '}')
 
     player_start_df = df[player_start_table]
-    player_start_df.rename(columns={"initial_hand": "player_initial"})
+    player_start_df.rename(columns={"initial_hand": "player_initial"}) # renaming to make it more clear
     player_start_df.loc[:, 'initial_hand'] = player_start_df['initial_hand'].apply(ast.literal_eval)
     player_start_df.loc[:, 'initial_hand'] = player_start_df['initial_hand'].astype(str).str.replace('[', '{').str.replace(']', '}')
 
